bdc415afed280d5f786612ee5c3c3a883ed0d162
박지현 <jh.park@tg360tech.com>
2023-10-12 15:39:42 +0900

    init

---
+++ src/main/kotlin/sample/jpa/SampleJpaKotlinApplication.kt
@@ -0,0 +1,41 @@
+package sample.jpa
+
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport.ConditionAndOutcome
+import org.springframework.boot.autoconfigure.condition.ConditionEvaluationReport.ConditionAndOutcomes
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.function.Consumer
+
+@SpringBootApplication
+class SampleJpaKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SampleJpaKotlinApplication>(*args)
+}
+
+@Bean
+fun run(report: ConditionEvaluationReport): ApplicationRunner {
+    return ApplicationRunner { args: ApplicationArguments? ->
+        report.getConditionAndOutcomesBySource().entries.stream()
+            .filter { (_, value): Map.Entry<String?, ConditionAndOutcomes> -> value.isFullMatch() }
+            .filter { (key): Map.Entry<String, ConditionAndOutcomes?> ->
+                !key.contains(
+                    "Jmx"
+                )
+            }
+            .forEach { (key, value): Map.Entry<String?, ConditionAndOutcomes> ->
+                println(key)
+                value.forEach(Consumer { c: ConditionAndOutcome ->
+                    println(
+                        "\t" + c.outcome
+                    )
+                })
+                println()
+            }
+    }
+}
---
+++ src/test/kotlin/sample/jpa/SampleJpaKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package sample.jpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SampleJpaKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
